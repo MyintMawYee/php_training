c25dbd75786efdc6153191f9d9ae710549184d59
MyintMawYee <64900459+MyintMawYee@users.noreply.github.com>
2022-04-21 16:19:25 +0630

    Updated tutorial 1 & 2

--- tutorial_1/chess.php
+++ tutorial_1/chess.php
@@ -4,20 +4,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
   <title>Chessboard Tutorial</title>
 </head>
 <body>
-  <h1 style="color: blue;">Chessboard Using PHP</h1>
-   <table style="border: 3px solid black;">
+  <h1>Chessboard Using PHP</h1>
+   <table>
      <?php
-        for($row = 1; $row <= 8; $row++){
+        for($row = 1; $row <= 8; $row++) {
           echo "<tr>";
-          for($col = 1; $col <= 8; $col++){
+          for($col = 1; $col <= 8; $col++) {
             $total = $row + $col;
             if($total % 2 == 0){
-              echo '<td style="width: 30px; height:30px; background-color: #fff;"></td>';
+              echo '<td class="td-white"></td>';
             } else{
-              echo '<td style="width: 30px; height:30px; background-color: #000;"></td>';
+              echo '<td class="td-black"></td>';
             }
           }
           echo "</tr>";
--- tutorial_2/diamond.php
+++ tutorial_2/diamond.php
@@ -6,21 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Diamond Tutorial</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<style>
-.container {
-    width: 400px;
-    margin: 0 auto;
-    text-align: center;
-}
-
-h1 {
-    text-align: center;
-    margin: 30px 0;
-    color: blue;
-}
-</style>
-
 <body class="container">
   <h1>Diamond Tutorial</h1>
   <?php 
@@ -49,5 +36,4 @@ h1 {
 
   ?>
 </body>
-
 </html>
