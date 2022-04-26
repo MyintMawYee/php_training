8e625f14563162d58f22e0c47d5ea1f5ec2f47d8
MyintMawYee <64900459+MyintMawYee@users.noreply.github.com>
2022-04-26 10:21:38 +0630

    Upload tutorial 6

---
+++ tutorial_6/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Upload</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <div class="btn">
+        <input type="submit" value="Upload Image" name="submit">
+    </div>
+</form>
+</body>
+</html>
